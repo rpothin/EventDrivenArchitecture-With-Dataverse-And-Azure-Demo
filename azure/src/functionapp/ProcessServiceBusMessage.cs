f38138bb198010c2a2f4b7f56e0a0698f5b2ab3a
rpothin <raphael.pothin@gmail.com>
2022-10-24 00:09:00 -0400

    Update Azure Functions app code to be able to create a contact in a Dataverse environment on the reception of a message in the right format in an ASB queue

--- azure/src/functionapp/ProcessServiceBusMessage.cs
+++ azure/src/functionapp/ProcessServiceBusMessage.cs
@@ -1,11 +1,11 @@
 using System;
 using Microsoft.Azure.WebJobs;
-//using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-//using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Newtonsoft.Json;
 
 namespace azd.Dataverse.Function
 {
@@ -17,6 +17,12 @@ namespace azd.Dataverse.Function
     public class ProcessServiceBusMessage
     {
         private readonly ServiceClient _client;
+
+        private struct ContactMessage {
+            public string firstName;
+            public string lastName;
+            public string email;
+        }; 
 
         public ProcessServiceBusMessage()
         {
@@ -33,9 +39,19 @@ namespace azd.Dataverse.Function
         }
 
         [FunctionName("ProcessServiceBusMessage")]
-        public void Run([ServiceBusTrigger("dataverse-inbound", Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
+        public void Run([ServiceBusTrigger("dataverse-inbound-function", Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
         {
+            /* Expected message format
+            {
+                "firstName": "Test",
+                "lastName": "Rpo 20221023 01",
+                "email": "test.rpo.2022102301@test.com"
+            }
+            */
+
+            // Deserialize the message
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            ContactMessage contactMessage = JsonConvert.DeserializeObject<ContactMessage>(myQueueItem);
 
             // Send a WhoAmI request to obtain information about the logged on user
             WhoAmIResponse whoAmIResponse = new WhoAmIResponse();
@@ -44,6 +60,16 @@ namespace azd.Dataverse.Function
             string userId = whoAmIResponse.UserId.ToString();
 
             log.LogInformation($"Logged on user id: {userId}");
+
+            // Create a new contact
+            Entity contact = new Entity("contact");
+            contact["firstname"] = contactMessage.firstName;
+            contact["lastname"] = contactMessage.lastName;
+            contact["emailaddress1"] = contactMessage.email;
+
+            Guid contactId = _client.Create(contact);
+
+            log.LogInformation($"Contact created with id: {contactId.ToString()}");
         }
     }
 }
